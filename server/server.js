7f69d4372aa7d2b0bf0cb3c992e9c29df6300c5e
李卓峰 <zhuofeng.li@lejent.com>
2017-03-10 14:47:57 +0800

    fix port

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-var path    = require('path');
+var path = require('path');
 var webpack = require('webpack');
 var express = require('express');
-var config  = require('../webpack.config');
+var config = require('../webpack.config');
 
-var app      = express();
+var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-	publicPath: '/public'
+    publicPath: '/public'
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
 });
 
-app.listen(3344, function (err) {
-	if (err) {
-		return console.error(err);
-	}
-	console.log('Listening at http://localhost:1180/');
+app.listen(3344, function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Listening at http://localhost:3344/');
 });
